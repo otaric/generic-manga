e8aa47a029b72d960bc7bae138c66e955f3945a0
William Marques <william.marques.pessoal@gmail.com>
2022-01-18 15:44:30 -0300

    alterações no HTML e uma correção no menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('[data-menu]')
 const iconMenu = document.querySelector('[data-icon-menu]')
 const iconClose = document.querySelector('[data-icon-close]')
+const itemMenu =document.querySelectorAll('[data-link-menu]')
 
 iconMenu.addEventListener('click', () => {
   menu.classList.remove('cabecalho__menu-hidden')
@@ -13,3 +14,11 @@ iconClose.addEventListener('click', () => {
   iconClose.classList.remove('show')
   iconMenu.classList.add('show')
 })
+
+for(let i=0; i<itemMenu.length; i++) {
+  itemMenu[i].addEventListener('click', () => {
+    menu.classList.add('cabecalho__menu-hidden')
+    iconClose.classList.remove('show')
+    iconMenu.classList.add('show')
+  })
+}
